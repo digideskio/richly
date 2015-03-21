e1eb6cd2c81ce6de1148e7fbdada08aafcd44da8
Richard Macias <RJNY86@gmail.com>
2015-03-21 09:49:00 -0400

    link order desc by created_at

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,7 +5,7 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @links = Link.all
+    @links = Link.all.order("created_at DESC")
   end
 
   # GET /links/1
